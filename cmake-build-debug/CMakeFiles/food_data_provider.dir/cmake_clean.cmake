17b83bb9207638b21fcc9941d91ec300765d2732
Sebastian Olaru <sebastianolaru3008@gmail.com>
2020-01-06 23:07:26 +0200

    Save data to file extra asignment

---
+++ cmake-build-debug/CMakeFiles/food_data_provider.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/food_data_provider.dir/main.c.obj"
+  "food_data_provider.exe"
+  "food_data_provider.exe.manifest"
+  "food_data_provider.pdb"
+  "libfood_data_provider.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/food_data_provider.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
